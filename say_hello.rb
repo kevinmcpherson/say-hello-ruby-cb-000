b119ca19dba24e4bcc5a8af33edfa3240b656746
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-09 22:55:27 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def greeting(name = "Ruby programmer")
+def say_hello(name = "Ruby programmer")
   puts "Hello, #{name}"
 end
 
-greeting "kevin!"
+say_hello "kevin!"
